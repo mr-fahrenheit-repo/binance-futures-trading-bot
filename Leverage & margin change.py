b1d4736161b62cd83e8f8d88f67d2038ac066ec9
Rifky Ashari <work@rifkyashari.com>
2022-11-14 19:15:16 +0700

    Adding Leverage & margin change

---
+++ Leverage & margin change.py
@@ -0,0 +1,11 @@
+from binance_client import leverage_change
+from binance_client import change_margin
+from binance_client import stock_list
+
+# Change Leverage to the highest for every pairs
+for x in stock_list():
+  leverage_change(x)
+  
+# Change Margin type to Isolated
+for x in stock:
+  change_margin(x)
