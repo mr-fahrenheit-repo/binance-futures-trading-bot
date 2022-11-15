250edfb4e80fe00bd593c7983afd3fa6790ecc83
Rifky Ashari <work@rifkyashari.com>
2022-11-15 10:11:17 +0700

    change take profit percentage

--- binance_client.py
+++ binance_client.py
@@ -101,7 +101,7 @@ mybalance = my_balance()
 caps = round(float(mybalance / 12.76), 2)  # float capital number number
 
 # Profit percentage per trade
-profit_percent = 66  # change this
+profit_percent = 5  # change this
 take_profit = round(float(profit_percent / 100), 1)
 
 
