18573103ae8fba595e8d710ab299ad2db18c21a9
Rifky Ashari <work@rifkyashari.com>
2022-11-16 13:08:10 +0700

    fix cancel active order

--- binance_client.py
+++ binance_client.py
@@ -12,7 +12,6 @@ binance_secret = os.getenv('binance_secret_key')
 # Set binance API Client
 client = Client(binance_api, binance_secret)
 
-
 # Getting all the stock with USDT pair symbol in a list
 def stock_list():
   info = client.futures_exchange_info()
@@ -22,7 +21,6 @@ def stock_list():
   stock_list = [x for x in stock_list if x[-4:] == 'USDT']
   return stock_list
 
-
 # Getting all the stock with non-USDT pair symbol in a list
 def non_stock_list():
   info = client.futures_exchange_info()
@@ -31,7 +29,6 @@ def non_stock_list():
   non_stock_list = [x for x in stock_list if x[-4:] != 'USDT']
   return non_stock_list
 
-
 # Stock list pair data information
 def pair_info():
   ex_info = client.futures_exchange_info()
@@ -39,9 +36,7 @@ def pair_info():
   df_filters = pd.DataFrame(list(df_ex_info['filters']))
   filters = pd.DataFrame(list(df_filters[0]))
   tick = pd.DataFrame(list(df_filters[1]))
-  data = df_ex_info[[
-    'symbol', 'pricePrecision', 'quantityPrecision', 'timeInForce'
-  ]]
+  data = df_ex_info[['symbol', 'pricePrecision', 'quantityPrecision', 'timeInForce']]
   data = pd.concat([data, tick, filters], axis=1)
   pos_info = client.futures_position_information()
   df_pos_info = pd.DataFrame(pos_info)
@@ -52,14 +47,9 @@ def pair_info():
   data = pd.concat([data, df_pos_info], axis=1)
   data = data.loc[:, ~data.columns.duplicated()].copy()
   data = data.set_index(data['symbol'])
-  data = data.drop([
-    'symbol', 'filterType', 'stepSize', 'timeInForce', 'maxQty', 'minQty',
-    'minPrice', 'maxPrice', 'tickSize'
-  ],
-                   axis=1)
+  data = data.drop(['symbol', 'filterType', 'stepSize', 'timeInForce', 'maxQty', 'minQty','minPrice', 'maxPrice', 'tickSize'], axis=1)
   data = data.drop(non_stock_list())
   return data
-
 
 # pairs leverage functions
 def call_leverage(symbol):
@@ -68,20 +58,17 @@ def call_leverage(symbol):
   x = int(df.loc[symbol, 'leverage'])
   return x
 
-
 # price precission functions
 def price_precision(symbol):
   x = pair_info()
   x = int(x.loc[symbol]['pricePrecision'])
   return x
 
-
 # quantity precission size functions
 def quantity_precision(symbol):
   x = pair_info()
   x = int(x.loc[symbol]['quantityPrecision'])
   return x
-
 
 # Calling for account balance
 def my_balance():
@@ -100,7 +87,6 @@ caps = round(float(my_balance() / 10), 2)  # float capital number number
 profit_percent = 5  # change this
 take_profit = round(float(profit_percent / 100), 1)
 
-
 # Confirming caps is enough for the trades
 def caps_confirm():
   if (my_balance() - total_margin()) > caps:
@@ -108,13 +94,11 @@ def caps_confirm():
   else:
     return False
 
-
 # Market price functions
 def market_price(symbol):
   x = client.futures_ticker(symbol=symbol)
   x = round(float(x['lastPrice']), price_precision(symbol))
   return x
-
 
 # Change leverage for every stock pair
 def leverage_change(symbol):
@@ -129,7 +113,6 @@ def leverage_change(symbol):
 
   return leverage
 
-
 # Change Margin Type for every stock pair
 def change_margin(symbol):
   try:
@@ -140,7 +123,6 @@ def change_margin(symbol):
     print('Change margin type Success')
     print('{} margin type is ISOLATED'.format(symbol))
 
-
 # function for defining quantity
 def quantities(price, symbol):
   leverage = call_leverage(symbol)
@@ -149,94 +131,67 @@ def quantities(price, symbol):
   quantity = round(float(quantity / price), quantity_precision(symbol))
   return quantity
 
-
 #Total margin in active position
 def total_margin():
   x = client.futures_position_information()
   df = pd.DataFrame(x)
-  df[['unRealizedProfit',
-      'isolatedWallet']] = df[['unRealizedProfit',
-                               'isolatedWallet']].astype(float)
+  df[['unRealizedProfit','isolatedWallet']] = df[['unRealizedProfit','isolatedWallet']].astype(float)
   df = df[['symbol', 'unRealizedProfit', 'isolatedWallet']]
   df = df.loc[df['isolatedWallet'].values > 0]
   margin = df['isolatedWallet'].sum()
   return margin
 
-
 # Total UnRealized Profit in active position
 def total_pnl():
   x = client.futures_position_information()
   df = pd.DataFrame(x)
-  df[['unRealizedProfit',
-      'isolatedWallet']] = df[['unRealizedProfit',
-                               'isolatedWallet']].astype(float)
+  df[['unRealizedProfit','isolatedWallet']] = df[['unRealizedProfit','isolatedWallet']].astype(float)
   df = df[['symbol', 'unRealizedProfit', 'isolatedWallet']]
   df = df.loc[df['isolatedWallet'].values > 0]
   pnl = df['unRealizedProfit'].sum()
   return pnl
 
-
 # Order quantity in active position
 def order_quantity(symbol):
   x = client.futures_position_information()
   df = pd.DataFrame(x)
-  df[['positionAmt', 'unRealizedProfit', 'isolatedWallet'
-      ]] = df[['positionAmt', 'unRealizedProfit',
-               'isolatedWallet']].astype(float)
+  df[['positionAmt', 'unRealizedProfit', 'isolatedWallet']] = df[['positionAmt', 'unRealizedProfit','isolatedWallet']].astype(float)
   df = df[['symbol', 'positionAmt', 'unRealizedProfit', 'isolatedWallet']]
-  df = df.loc[df['isolatedWallet'].values > 0]
   df = df.set_index(df['symbol'])
-  quantity = round(float(df['positionAmt'][symbol]),
-                   quantity_precision(symbol))
+  quantity = round(float(df['positionAmt'][symbol]))
   return quantity
-
 
 # Order price in active position
 def order_price(symbol):
   x = client.futures_position_information()
   df = pd.DataFrame(x)
-  df[['positionAmt', 'entryPrice', 'unRealizedProfit',
-      'isolatedWallet']] = df[[
-        'positionAmt', 'entryPrice', 'unRealizedProfit', 'isolatedWallet'
-      ]].astype(float)
-  df = df[[
-    'symbol', 'positionAmt', 'entryPrice', 'unRealizedProfit', 'isolatedWallet'
-  ]]
+  df[['positionAmt', 'entryPrice', 'unRealizedProfit','isolatedWallet']] = df[['positionAmt', 'entryPrice', 'unRealizedProfit', 'isolatedWallet']].astype(float)
+  df = df[['symbol', 'positionAmt', 'entryPrice', 'unRealizedProfit', 'isolatedWallet']]
   df = df.loc[df['isolatedWallet'].values > 0]
   df = df.set_index(df['symbol'])
   price = round(float(df['entryPrice'][symbol]), price_precision(symbol))
   return price
 
-
 # check the order
 def check_order(symbol):
   x = client.futures_position_information()
   df = pd.DataFrame(x)
-  df[['positionAmt', 'entryPrice', 'unRealizedProfit',
-      'isolatedWallet']] = df[[
-        'positionAmt', 'entryPrice', 'unRealizedProfit', 'isolatedWallet'
-      ]].astype(float)
-  df = df[[
-    'symbol', 'positionAmt', 'entryPrice', 'unRealizedProfit', 'isolatedWallet'
-  ]]
+  df[['positionAmt', 'entryPrice', 'unRealizedProfit','isolatedWallet']] = df[['positionAmt', 'entryPrice', 'unRealizedProfit', 'isolatedWallet']].astype(float)
+  df = df[['symbol', 'positionAmt', 'entryPrice', 'unRealizedProfit', 'isolatedWallet']]
   df = df.loc[df['isolatedWallet'].values > 0]
   df = df.set_index(df['symbol'])
   check = symbol in df.index
   return check
 
-
 # List of active order
 def order_list():
   x = client.futures_position_information()
   df = pd.DataFrame(x)
-  df[['positionAmt', 'unRealizedProfit', 'isolatedWallet'
-      ]] = df[['positionAmt', 'unRealizedProfit',
-               'isolatedWallet']].astype(float)
+  df[['positionAmt', 'unRealizedProfit', 'isolatedWallet']] = df[['positionAmt', 'unRealizedProfit','isolatedWallet']].astype(float)
   df = df[['symbol', 'positionAmt', 'unRealizedProfit', 'isolatedWallet']]
   df = df.loc[df['isolatedWallet'].values > 0]
   order_list = list(df['symbol'])
   return order_list
-
 
 # create list of every open order
 def open_order_list():
@@ -245,12 +200,10 @@ def open_order_list():
   open_list = list(df['symbol'])
   return open_list
 
-
 # canceling all open order list
 def cancel_open_order():
   for i in open_order_list():
     client.futures_cancel_all_open_orders(symbol=i)
-
 
 # Create Buy Order functions
 def cancel_buy_order(symbol):
@@ -260,7 +213,6 @@ def cancel_buy_order(symbol):
                                       quantity=order_quantity(symbol))
   return order
 
-
 # Create Sell Order functions
 def cancel_sell_order(symbol):
   order = client.futures_create_order(symbol=symbol,
@@ -269,26 +221,21 @@ def cancel_sell_order(symbol):
                                       quantity=order_quantity(symbol))
   return order
 
-
 # Create Buy Order functions
 def buy_order(symbol):
   order = client.futures_create_order(symbol=symbol,
                                       side='BUY',
                                       type='MARKET',
-                                      quantity=quantities(
-                                        market_price(symbol), symbol))
+                                      quantity=quantities(market_price(symbol), symbol))
   return order
-
 
 # Create Sell Order functions
 def sell_order(symbol):
   order = client.futures_create_order(symbol=symbol,
                                       side='SELL',
                                       type='MARKET',
-                                      quantity=quantities(
-                                        market_price(symbol), symbol))
+                                      quantity=quantities(market_price(symbol), symbol))
   return order
-
 
 # Take profit for sell order
 def sell_take_profit(symbol):
@@ -297,16 +244,11 @@ def sell_take_profit(symbol):
     side='BUY',
     type='TAKE_PROFIT',
     quantity=abs(order_quantity(symbol)),
-    price=round(
-      float(order_price(symbol) * (1 - (take_profit / call_leverage(symbol)))),
-      price_precision(symbol)),
-    stopPrice=round(
-      float(order_price(symbol) * (1 - (take_profit / call_leverage(symbol)))),
-      price_precision(symbol)),
+    price=round(float(order_price(symbol) * (1 - (take_profit / call_leverage(symbol)))),price_precision(symbol)),
+    stopPrice=round(float(order_price(symbol) * (1 - (take_profit / call_leverage(symbol)))),price_precision(symbol)),
     reduceOnly=True,
     timeInForce='GTC')
   return order
-
 
 # Stop loss for sell order
 def sell_stop_loss(symbol):
@@ -316,13 +258,10 @@ def sell_stop_loss(symbol):
     type='STOP',
     quantity=abs(order_quantity(symbol)),
     price=order_price(symbol),
-    stopPrice=round(
-      float(order_price(symbol) * (1 + (1 / call_leverage(symbol)))),
-      price_precision(symbol)),
+    stopPrice=round(float(order_price(symbol) * (1 + (1 / call_leverage(symbol)))),price_precision(symbol)),
     reduceOnly=True,
     timeInForce='GTC')
   return order
-
 
 # Take profit for buy order
 def buy_take_profit(symbol):
@@ -331,16 +270,11 @@ def buy_take_profit(symbol):
     side='SELL',
     type='TAKE_PROFIT',
     quantity=abs(order_quantity(symbol)),
-    price=round(
-      float(order_price(symbol) * (1 + (take_profit / call_leverage(symbol)))),
-      price_precision(symbol)),
-    stopPrice=round(
-      float(order_price(symbol) * (1 + (take_profit / call_leverage(symbol)))),
-      price_precision(symbol)),
+    price=round(float(order_price(symbol) * (1 + (take_profit / call_leverage(symbol)))),price_precision(symbol)),
+    stopPrice=round(float(order_price(symbol) * (1 + (take_profit / call_leverage(symbol)))),price_precision(symbol)),
     reduceOnly=True,
     timeInForce='GTC')
   return order
-
 
 # Stop loss for buy order
 def buy_stop_loss(symbol):
@@ -350,9 +284,7 @@ def buy_stop_loss(symbol):
     type='STOP',
     quantity=abs(order_quantity(symbol)),
     price=order_price(symbol),
-    stopPrice=round(
-      float(order_price(symbol) * (1 - (1 / call_leverage(symbol)))),
-      price_precision(symbol)),
+    stopPrice=round(float(order_price(symbol) * (1 - (1 / call_leverage(symbol)))),price_precision(symbol)),
     reduceOnly=True,
     timeInForce='GTC')
   return order
@@ -375,16 +307,10 @@ def cancel_active_order():
 # Get the the data for 24 hours period
 def data_fetcher(stock_name):
   # fetch the data from binance
-  crypto = client.futures_klines(symbol=stock_name,
-                                 interval=client.KLINE_INTERVAL_5MINUTE,
-                                 limit=308)
+  crypto = client.futures_klines(symbol=stock_name, interval=client.KLINE_INTERVAL_5MINUTE, limit=308)
 
   # Make columns names
-  columns = [
-    'datetime', 'open', 'high', 'low', 'close', 'volume', 'close time',
-    'quote asset volume', 'number of trade', 'taker buy base', 'taker buy',
-    'ignore'
-  ]
+  columns = ['datetime', 'open', 'high', 'low', 'close', 'volume', 'close time','quote asset volume', 'number of trade', 'taker buy base', 'taker buy','ignore']
 
   # Convert the data into dataframe
   df = pd.DataFrame(crypto, columns=columns)
@@ -393,8 +319,7 @@ def data_fetcher(stock_name):
   df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
 
   # Convert OHLC columns into float
-  df[['open', 'high', 'low', 'close']] = df[['open', 'high', 'low',
-                                             'close']].astype(float)
+  df[['open', 'high', 'low', 'close']] = df[['open', 'high', 'low','close']].astype(float)
 
   # set datetime column as index
   df = df.set_index('datetime')
@@ -410,11 +335,7 @@ def data_fetcher(stock_name):
   df['datetime'] = df['datetime'].dt.tz_localize(None)
 
   #Drop unnecessary columns
-  df = df.drop([
-    'volume', 'close time', 'quote asset volume', 'number of trade',
-    'taker buy base', 'taker buy', 'ignore'
-  ],
-               axis=1)
+  df = df.drop(['volume', 'close time', 'quote asset volume', 'number of trade','taker buy base', 'taker buy', 'ignore'],axis=1)
 
   # set datetime column as index
   df = df.set_index('datetime')
